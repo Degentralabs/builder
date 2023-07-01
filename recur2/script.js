2bf1e440e2fb560721b0b9576ff95f732c6a04ab
Degentralabs <133314487+Degentralabs@users.noreply.github.com>
2023-07-01 17:23:12 -0600

    Update script.js

--- recur2/script.js
+++ recur2/script.js
@@ -3,7 +3,7 @@ var backgrounds = [
     { name: 'Blue', image: 'https://imagizer.imageshack.com/img924/6237/cFZTzY.png' }
 ];
 
-var Styles = [
+var styles = [
     { name: 'Ape', image: 'https://imagizer.imageshack.com/img924/918/uJtYfd.png' },
     { name: 'Zombie', image: 'https://imagizer.imageshack.com/img922/4089/DOiRVT.png' }
 ];
@@ -18,77 +18,86 @@ function updateBackgrounds() {
     backgroundsDiv.innerHTML = '';
 
     for (var i = 0; i < backgrounds.length; i++) {
-        var option = document.createElement('div');
-        option.className = 'category-option';
-        option.onclick = function() {
-            document.getElementById('background-image').src = this.dataset.image;
-            document.getElementById('confirm-button').style.display = 'block'; // Show the confirm button
-        };
-        option.dataset.image = backgrounds[i].image;
-
+        var div = document.createElement('div');
+        div.className = 'category-option';
+    
         var img = document.createElement('img');
         img.src = backgrounds[i].image;
-        img.alt = backgrounds[i].name;
-
-        option.appendChild(img);
-        backgroundsDiv.appendChild(option);
-    }
-}
-
-function updateStyles() {
-var StylesDiv = document.getElementById('styles');
-StylesDiv.innerHTML = '';
-
-for (var i = 0; i < Styles.length; i++) {
-var option = document.createElement('div');
-option.className = 'category-option';
-option.onclick = function() {
-    document.getElementById('body-image').src = this.dataset.image;
-    document.getElementById('body-image').style.display = 'block'; // Add this line
-    document.getElementById('confirm-button').style.display = 'block'; // Show the confirm button
-};
-option.dataset.image = Styles[i].image;
-
-var img = document.createElement('img');
-img.src = Styles[i].image;
-img.alt = Styles[i].name;
-
-option.appendChild(img);
-StylesDiv.appendChild(option);
-}
-
-StylesDiv.parentElement.style.display = 'block'; // Show the body style options
+    
+        // Add event listener to the image
+        img.addEventListener('click', function() {
+            // Get the compiler image element
+            var compilerImage = document.getElementById('background-image');
+    
+            // Set the source of the compiler image to the source of the clicked image
+            compilerImage.src = this.src;
+        });
+    
+        div.appendChild(img);
+        backgroundsDiv.appendChild(div);
+    }       
 }
 
 window.addEventListener('DOMContentLoaded', function() {
     updateBackgrounds();
-    updateStyles();
-});
+    // ... any other initialization code ...
+});   
+
+    function updateStyles() {
+        var stylesDiv = document.getElementById('styles-grid'); // Change 'styles' to 'styles-grid'
+        stylesDiv.innerHTML = '';
+    
+        for (var i = 0; i < Styles.length; i++) {
+            var img = document.createElement('img');
+            img.src = Styles[i].image;
+            stylesDiv.appendChild(img);
+        
+            console.log('Styles:', document.getElementById('styles-grid').innerHTML);
+            console.log('Style images:', styles.map(function(style) { return style.image; }));
+        }    
+    } // Closing bracket for updateStyles function
+    
+    window.addEventListener('DOMContentLoaded', function() {
+        updateBackgrounds();
+        updateStyles();
+        
+        // Show content for default tab
+        var defaultTabContent = document.querySelector('.tab-content > div[data-tabby-default]');
+        if (defaultTabContent) {
+            defaultTabContent.style.display = 'block';
+        }
+    }); // Closing bracket for event listener    
 
 var tabs = new Tabby('[data-tabs]');
 
     // Add event listener for tab switching
-    var tabLinks = document.querySelectorAll('.tabs a');
-    tabLinks.forEach(function(tabLink) {
-        tabLink.addEventListener('click', function(event) {
-            event.preventDefault();
-    
-            // Remove active class from all tabs
-            tabLinks.forEach(function(tabLink) {
-                tabLink.classList.remove('active');
-            });
-    
-            // Add active class to clicked tab
-            this.classList.add('active');
-    
-            // Hide all tab content
-            var tabContents = document.querySelectorAll('.tab-content > div');
-            tabContents.forEach(function(tabContent) {
-                tabContent.style.display = 'none';
-            });
-    
-            // Show content for clicked tab
-            var tabContent = document.querySelector(this.getAttribute('href'));
-            tabContent.style.display = 'block';
+// Add event listener for tab switching
+var tabLinks = document.querySelectorAll('.tabs a');
+tabLinks.forEach(function(tabLink) {
+    tabLink.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        // Remove active class from all tabs
+        tabLinks.forEach(function(tabLink) {
+            tabLink.classList.remove('active');
         });
-    });    
+
+        // Add active class to clicked tab
+        this.classList.add('active');
+
+        // Hide all tab content
+        var tabContents = document.querySelectorAll('.tab-content > div');
+        tabContents.forEach(function(tabContent) {
+            tabContent.style.display = 'none';
+        });
+
+        // Show content for clicked tab
+        var tabContent = document.querySelector(this.getAttribute('href'));
+        tabContent.style.display = 'block';
+
+        // If the "Backgrounds" tab is clicked, update the backgrounds
+        if (this.getAttribute('href') === '#backgrounds') {
+            updateBackgrounds();
+        }
+    });
+});   
